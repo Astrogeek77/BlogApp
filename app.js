430893f7aafa23a83e64445a320351be7eb3e7b5
Gautam Jain <54165628+Astrogeek77@users.noreply.github.com>
2020-05-22 00:36:02 +0530

    update app.listen

--- app.js
+++ app.js
@@ -141,6 +141,7 @@ function escapeRegex(text) {
 };
 
 
-app.listen(process.env.port || 3000, process.env.IP || 0.0.0.0/0, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log("Server has Started!");
 })
+//  process.env.IP || 0.0.0.0/0,
